defa8fa038e17805e004e08514c4dd19339e9f19
Andy Wang <wang.andy32@gmail.com>
2025-04-27 13:23:46 +0000

    added material ui card and also buttons to assist in swiping cards

--- src/components/CardSwipe.tsx
+++ src/components/CardSwipe.tsx
@@ -1,22 +1,89 @@
-import React from 'react';
+import React, { createRef } from 'react';
 import TinderCard from 'react-tinder-card'
+import { Card, CardContent, Typography, IconButton, Box } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
+import CheckIcon from '@mui/icons-material/Check';
+
+
 
 
 const CardSwipe = () => {
-    const onSwipe = (direction) => {
-        console.log('You swiped: ' + direction)
-    }
+
+  const inputRef = createRef<{ swipe: (dir: string) => void; restoreCard: () => void }>();
+
+
+  const onSwipe = (direction: string) => {
+    console.log('You swiped: ' + direction)
+  }
       
-    const onCardLeftScreen = (myIdentifier) => {
-        console.log(myIdentifier + ' left the screen')
-    }
+  const onCardLeftScreen = (myIdentifier: string) => {
+    console.log(myIdentifier + ' left the screen')
+  }
+
+  const swipe = async (dir: string) => {
+    await inputRef?.current?.swipe(dir)
+  }  
       
   return (
-    <TinderCard 
+    <TinderCard
+        ref={inputRef as React.RefObject<any>}
         onSwipe={onSwipe} 
         onCardLeftScreen={() => onCardLeftScreen('fooBar')} 
         preventSwipe={['right', 'left']}>
-        Hello, World!
+        <Card className='inner-card'>
+          <CardContent>
+              <Typography variant="h5" component="div">
+                Minimalistic Card
+              </Typography>
+            <Typography variant="body2" color="text.secondary">
+              This is a simple card from Material-UI.
+            </Typography>
+          </CardContent>
+        </Card>
+        <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        width: '200px',
+        margin: 'auto',
+        padding: 2,
+      }}
+    >
+      {/* X Button (left) */}
+      <IconButton
+        sx={{
+          backgroundColor: 'error.main',
+          color: 'white',
+          width: 60,
+          height: 60,
+          borderRadius: '50%',
+          boxShadow: 3,
+          '&:hover': {
+            backgroundColor: 'error.dark',
+          },
+        }}
+        onClick={() => swipe('left')}
+      >
+        <CloseIcon fontSize="large" />
+      </IconButton>
+
+      <IconButton
+        sx={{
+          backgroundColor: 'success.main',
+          color: 'white',
+          width: 60,
+          height: 60,
+          borderRadius: '50%',
+          boxShadow: 3,
+          '&:hover': {
+            backgroundColor: 'success.dark',
+          },
+        }}
+        onClick={() => swipe('right')}
+      >
+        <CheckIcon fontSize="large" />
+      </IconButton>
+    </Box>
     </TinderCard>
   )
 }
